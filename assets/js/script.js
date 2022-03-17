6d4c1eaa65bfa099a1b2461df06507585f1d0b12
Sejung Kwak <kwak.sejung@gmail.com>
2022-03-17 17:01:02 +0000

    feat: display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ function getButtons() {
   for (let button of buttons) {
     button.addEventListener('click', clickHandler);
   }
+
+  runGame('addition');
 }
 
 function clickHandler(e) {
@@ -17,7 +19,7 @@ function clickHandler(e) {
   if (buttonType === 'submit') {
     alert('You clicked submit!');
   } else {
-    alert(`You clicked ${buttonType}`);
+    runGame(buttonType);
   }
 }
 
@@ -26,11 +28,18 @@ function clickHandler(e) {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
   // creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === 'addition') {
+    displayAdditionQuiestion(num1, num2);
+  } else {
+    alert(`unknown game type: ${gameType}`);
+    throw `unknown game type: ${gameType}. Aborting!`;
+  }
 }
 
 function checkAnswer() {
@@ -49,8 +58,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuiestion() {
-
+function displayAdditionQuiestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
