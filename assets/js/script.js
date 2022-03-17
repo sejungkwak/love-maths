8b508dae1c7e1d97743c1ee4e8d9b8f41850ad23
Sejung Kwak <kwak.sejung@gmail.com>
2022-03-17 16:22:46 +0000

    feat: add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+// wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener('DOMContentLoaded', getButtons);
+
+function getButtons() {
+  let buttons = document.getElementsByTagName('button');
+
+  for (let button of buttons) {
+    button.addEventListener('click', clickHandler);
+  }
+}
+
+function clickHandler(e) {
+  let buttonType = e.currentTarget.getAttribute('data-type');
+
+  if (buttonType === 'submit') {
+    alert('You clicked submit!');
+  } else {
+    alert(`You clicked ${buttonType}`);
+  }
+}
+
 function runGame() {
 
 }
