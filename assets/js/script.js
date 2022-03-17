3a1ce02af649d7e30f123fc60bc2830b6e48683f
Sejung Kwak <kwak.sejung@gmail.com>
2022-03-17 17:14:59 +0000

    feat: add code to calculate correct answer for the addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus, etc)
+ * directly from the DOM, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+  let operand1 = +document.getElementById('operand1').innerText;
+  let operand2 = +document.getElementById('operand2').innerText;
+  let operator = document.getElementById('operator').innerText;
 
+  if (operator === '+') {
+    return [operand1 + operand2, 'addition'];
+  } else {
+    alert(`unimplemented operator ${operator}`);
+    throw `unimplemented operator ${operator}. Aborting!`;
+  }
 }
 
 function incrementScore() {
