c6b800cd61b99a92fef2fec5472122090425cf92
Sejung Kwak <kwak.sejung@gmail.com>
2022-03-17 23:53:21 +0000

    feat: add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,8 @@ function runGame(gameType) {
     displaySubtractQuestion(num1, num2);
   } else if (gameType === 'multiply') {
     displayMultiplyQuestion(num1, num2);
+  } else if (gameType === 'division') {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`unknown game type: ${gameType}`);
     throw `unknown game type: ${gameType}. Aborting!`;
@@ -91,6 +93,8 @@ function calculateCorrectAnswer() {
     return [operand1 - operand2, 'subtract'];
   } else if (operator === 'x') {
     return [operand1 * operand2, 'multiply'];
+  } else if (operator === '/') {
+    return [operand1 / operand2, 'division'];
   } else {
     alert(`unimplemented operator ${operator}`);
     throw `unimplemented operator ${operator}. Aborting!`;
@@ -120,8 +124,8 @@ function displayAdditionQuiestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-  document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operand1').textContent = operand1 >= operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand1 >= operand2 ? operand2 : operand1;
   document.getElementById('operator').textContent = '-';
 }
 
@@ -129,4 +133,10 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1 >= operand2 ? operand1 - (operand1 % operand2) : operand2 - (operand2 % operand1);
+  document.getElementById('operand2').textContent = operand1 >= operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = '/';
 }
