2a1b76fde813d942e0ca5d33789b05a4ffa06753
Sejung Kwak <kwak.sejung@gmail.com>
2022-03-17 22:16:00 +0000

    feat: add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,8 @@ function runGame(gameType) {
 
   if (gameType === 'addition') {
     displayAdditionQuiestion(num1, num2);
+  } else if (gameType === 'multiply') {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`unknown game type: ${gameType}`);
     throw `unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +75,8 @@ function calculateCorrectAnswer() {
 
   if (operator === '+') {
     return [operand1 + operand2, 'addition'];
+  } else if (operator === 'x') {
+    return [operand1 * operand2, 'multiply'];
   } else {
     alert(`unimplemented operator ${operator}`);
     throw `unimplemented operator ${operator}. Aborting!`;
@@ -105,6 +109,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = 'x';
 }
